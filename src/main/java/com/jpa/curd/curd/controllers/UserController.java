ea9ca9f19a92948e39472ef02ff8e5ab9cba932e
foysalahmmed <foysalahmmed73@gmail.com>
2020-04-06 21:00:18 +0600

    Get All user

--- src/main/java/com/jpa/curd/curd/controllers/UserController.java
+++ src/main/java/com/jpa/curd/curd/controllers/UserController.java
@@ -1,8 +1,23 @@
 package com.jpa.curd.curd.controllers;
 
+import com.jpa.curd.curd.domains.User;
+import com.jpa.curd.curd.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class UserController {
+import java.util.List;
 
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    UserService userService;
+    @GetMapping
+    public Page<User> getAllUser(Pageable pageable){
+       return userService.getAllUser(pageable);
+    }
 }
--- src/main/java/com/jpa/curd/curd/services/UserService.java
+++ src/main/java/com/jpa/curd/curd/services/UserService.java
@@ -1,7 +1,20 @@
 package com.jpa.curd.curd.services;
 
+import com.jpa.curd.curd.domains.User;
+import com.jpa.curd.curd.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class UserService {
+    @Autowired
+    UserRepository userRepository;
+
+    public Page<User> getAllUser(Pageable pageable){
+        return userRepository.findAll(pageable);
+    }
+
 }
