ae31e2e82f1b841d95fa887bbf8c2a43c24d61d2
foysalahmmed <foysalahmmed73@gmail.com>
2020-04-06 19:44:07 +0600

    Add User class

---
+++ src/main/java/com/jpa/curd/curd/domains/User.java
@@ -0,0 +1,46 @@
+package com.jpa.curd.curd.domains;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User extends Audit{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String email;
+    private String passowrd;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassowrd() {
+        return passowrd;
+    }
+
+    public void setPassowrd(String passowrd) {
+        this.passowrd = passowrd;
+    }
+}
