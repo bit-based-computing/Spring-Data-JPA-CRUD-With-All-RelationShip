3d067aa07cb1b1ae47cc1ad9ea746de42ffe88e3
foysalahmmed <foysalahmmed73@gmail.com>
2020-04-07 06:20:22 +0600

    Update User

--- src/main/java/com/jpa/curd/curd/services/UserService.java
+++ src/main/java/com/jpa/curd/curd/services/UserService.java
@@ -1,6 +1,7 @@
 package com.jpa.curd.curd.services;
 
 import com.jpa.curd.curd.domains.User;
+import com.jpa.curd.curd.exceptions.ApplicationRunTimeException;
 import com.jpa.curd.curd.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -13,12 +14,21 @@ public class UserService {
     @Autowired
     UserRepository userRepository;
 
-    public Page<User> getAllUser(Pageable pageable){
+    public Page<User> getAllUser(Pageable pageable) {
         return userRepository.findAll(pageable);
     }
 
-    public User saveUser(User user){
+    public User saveUser(User user) {
         return userRepository.save(user);
+    }
+
+    public User updateUser(Long userId, User updatedUser) {
+        return userRepository.findById(userId).map(user ->{
+                user.setName(updatedUser.getName());
+                user.setEmail(updatedUser.getEmail());
+                user.setPassword(updatedUser.getPassword());
+                return userRepository.save(user);
+        }).orElseThrow(()->new ApplicationRunTimeException("userId " + userId +" Not Found"));
     }
 
 }
