73a819ded4c1589e20b216bc722a9bec74e9baf6
foysalahmmed <foysalahmmed73@gmail.com>
2020-04-11 08:51:17 +0600

    Add custom validation error response

---
+++ src/main/java/com/jpa/curd/curd/exceptions/CustomGlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.jpa.curd.curd.exceptions;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", new Date());
+        body.put("status", status.value());
+        Map<String,String> errors = new HashMap<>();
+        ex.getBindingResult()
+                .getFieldErrors()
+                .stream().forEach(message -> errors.put(message.getField(),message.getDefaultMessage()));
+
+        body.put("errors", errors);
+        return new ResponseEntity<>(body, headers, status);
+    }
+}
